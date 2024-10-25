62f4ad97fa4964efa7a858e57ad6dabc823369a1
NgokNgok04 <117185466+NgokNgok04@users.noreply.github.com>
2024-10-26 01:22:28 +0700

    feat: lighthouse Login and Register

--- php/src/App/Views/Auth/RegisterPage.php
+++ php/src/App/Views/Auth/RegisterPage.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
     <link rel="stylesheet" href="../../../public/styles/global.css">
     <link rel="stylesheet" href="../../../public/styles/navbar.css">
     <link rel="stylesheet" href="../../../public/styles/register.css">
