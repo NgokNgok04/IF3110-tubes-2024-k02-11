68ae0eafdbfd85f88694db2e1be751a0f87d4755
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2024-10-27 00:47:27 +0700

    fix: fix detailLowongan view for mobile

--- php/src/App/Views/Company/DetailLowongan.php
+++ php/src/App/Views/Company/DetailLowongan.php
@@ -15,38 +15,48 @@
 <body>
     <?php
     include(__DIR__ . "/../../Components/navbar.php");
-    generateNavbar('Company')
-        ?>
-    <main>
-        <table class="detail-container">
-            <thead class="detail-table-head">
-                <tr>
-                    <th style="width: 200px;">Name</th>
-                    <th style="width: 100px;">Status</th>
-                    <th style="width: 100px;">Details</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (!empty($listLamaran)): ?>
-                    <?php foreach ($listLamaran as $lamaran): ?>
-                        <tr class="detail-table-row">
-                            <td class="tdata-nama" style="width: 200px;"><?= htmlspecialchars($lamaran['nama']) ?></td>
-                            <td style="width: 100px;" class="status <?= strtolower($lamaran['status']) ?>">
-                                <?= htmlspecialchars($lamaran['status']) ?>
-                            </td>
-                            <td style="width: 100px;">
-                                <a class="detail-action" href="/detail-lamaran/<?= $lamaran['lamaran_id'] ?>">Detail</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else: ?>
+    generateNavbar('Company');
+    ?>
+
+    <main class="content-container">
+
+        <div class="table-wrapper">
+            <a href="/export-applicants/<?= $lowongan['lowongan_id'] ?>" class="detail-action export-button"
+                aria-label="Export applicant list">Export</a>
+            <table class="detail-container">
+                <thead class="detail-table-head">
                     <tr>
-                        <td colspan="3">No Applications for this Job.</td>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Details</th>
                     </tr>
-                <?php endif; ?>
-            </tbody>
-        </table>
+                </thead>
+                <div class="table-body-wrapper">
+                    <tbody class="detail-table-body">
+                        <?php if (!empty($listLamaran)): ?>
+                                <?php foreach ($listLamaran as $lamaran): ?>
+                                    <tr class="detail-table-row">
+                                        <td class="tdata-nama"><?= htmlspecialchars($lamaran['nama']) ?></td>
+                                        <td class="status <?= strtolower($lamaran['status']) ?>">
+                                            <?= htmlspecialchars($lamaran['status']) ?>
+                                        </td>
+                                        <td>
+                                            <a class="detail-action" href="/detail-lamaran/<?= $lamaran['lamaran_id'] ?>"
+                                                aria-label="View details for <?= htmlspecialchars($lamaran['nama']) ?>">Detail</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                        <?php else: ?>
+                            <tr>
+                                <td colspan="3">No Applications for this Job.</td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </div>
+            </table>
+        </div>
     </main>
+
     <script src="../../../public/js/detail-lowongan.js"></script>
 </body>
 
